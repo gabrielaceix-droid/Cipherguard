3b0271183ddac92c019c3d727092276ad747afbb
gabrielaceix-droid <gabriela.ceix@gmail.com>
2026-05-13 21:58:12 -0600

    app.py

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def modulo4():
     return render_template('modulo4.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
